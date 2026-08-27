61448f91fd4b2aacfb2805d243ac8d527fa34ce3
shanduy <elpavosth@gmail.com>
2026-08-27 17:01:59 -0500

    Arreglos basicos de carpetas

---
+++ archivos/menus/main.js
@@ -0,0 +1,30 @@
+const getMainMenu = () => {
+    return `─── *Sabrina 💋* ───
+
+📌 *MENÚ PRINCIPAL DE COMANDOS*
+
+Escribe el comando del submenú para ver los detalles:
+
+🤖 \`!sam\` - Control del bot
+🏷️ \`!sm\` - Stickers y Herramientas
+👥 \`!am\` - Menú de Grupo (Administración)
+🚫 \`!mo\` - Restringir el bot a solo admins (Administración)
+🎵 \`!dm\` - Descarga de Audio (MP3)
+🔞 \`!nm\` - Contenido NSFW (+18)
+🔥 \`!em\` - Interacciones
+🎲 \`!jm\` - Juegos
+🎰 \`!cm\` - Casino
+
+
+📢 *CANAL OFICIAL Y REPOSITORIO*
+✅ \`!canal\` - Canal de whatsapp de Sabrina
+✅ \`!repo\` - Repositorio Oficial
+✅ \`!creador\` - Contacto personal
+
+
+> *Sabrina v1.1*
+> 👑 *Creador:*
+> https://instagram.com/shaanduy`.trim();
+};
+
+module.exports = { getMainMenu };
